efff6c62ba40ee76fcfce401a62aca9f26f744b1
Jacob Sander <jacob.sander@carnegie.dk>
2019-09-25 23:38:21 +0200

    2

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import jokes from "./jokes";
+
+// const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+// document.getElementById("jokes").innerHTML = allJokes.join("");
+// function getJoke() {
+//   document.getElementById("myJoke").innerHTML = jokes.getJokeById(document.getElementById("jokeId").value);
+// }
+// window.getJoke = getJoke;
+
+// function addJoke() {
+//   jokes.addJoke(document.getElementById("joke").value);
+//   document.getElementById("jokes").innerHTML = jokes.getJokes().map(joke => "<li>"+joke+"</li>").join("");
+// }
+// window.addJoke = addJoke;
+
+  document.getElementById("myButt").addEventListener("click", myGetRemoteJoke);
+
+   async function GetRemoteJoke() {
+       let response = await fetch('https://studypoints.info/jokes/api/jokes/period/hour');
+       let data = await response.json();
+       return data;
+   }
+
+  function myGetRemoteJoke() {
+    alert("1");
+    GetRemoteJoke().then(data => document.getElementById("myDiv").innerHTML = JSON.stringify(data));
+  }
